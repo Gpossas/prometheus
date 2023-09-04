9b61cf5e8d58af011228652302230395e45314da
Gpossas <gpossas@protonmail.com>
2023-09-04 09:01:56 -0300

    driver start when page is loaded instead of when search for video driver doesn't open window

--- api/web_driver_singleton.py
+++ api/web_driver_singleton.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 class WebDriverSingleton:
   _instance = None
 
   def __new__( cls ):
     if cls._instance is None:
-      service = webdriver.Chrome( service=Service( ChromeDriverManager().install() ) )
+      chrome_options = Options()
+      chrome_options.add_argument("--headless")
+      service = webdriver.Chrome( service=Service( ChromeDriverManager().install() ), options=chrome_options )
       cls._instance = service
     return cls._instance
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from api.web_driver_singleton import WebDriverSingleton
 
 # Create your views here.
 def index( request ):
+  # initiate driver
+  WebDriverSingleton()
   return render( request, "instagram/index.html" )
