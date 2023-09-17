cdb355f15baf792988d8fb6b7fd786e05cafe135
Gpossas <gpossas@protonmail.com>
2023-09-17 13:12:42 -0300

    update single ton to work with user sessions

--- api/web_driver_singleton.py
+++ api/web_driver_singleton.py
@@ -4,21 +4,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 class WebDriverSingleton:
-  _instance = None
+  _instances = {}
 
-  def __new__( cls ):
-    if cls._instance is None:
+  def __new__( cls, user_session ):
+    if user_session not in cls._instances:
       chrome_options = Options()
       chrome_options.add_argument("--headless")
       chrome_options.add_argument('--no-sandbox')
       chrome_options.add_argument('--disable-gpu')
       driver = webdriver.Chrome( service=Service( ChromeDriverManager().install() ), options=chrome_options )
       driver.implicitly_wait( 15 )
-      cls._instance = driver
-    return cls._instance
+      cls._instance[user_session] = driver
+    return cls._instance[user_session]
   
   @classmethod
-  def close_driver( cls ):
-    if cls._instance is not None:
-      cls._instance.quit()
-      cls._instance = None
+  def close_driver( cls, user_session ):
+    if user_session in cls._instances:
+      cls._instance[user_session].quit()
+      del cls._instance[user_session] 
