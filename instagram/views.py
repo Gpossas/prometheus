fb2a148b293e30d5d6a32b7634b5a2558db30e9f
slocksert <rsdlj01@gmail.com>
2023-09-18 12:02:09 -0300

    changing index view to create an instance only if a uuid is created

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
 from api.web_driver_singleton import WebDriverSingleton
+from instagram.utils import decode_and_set_cookie
 
-# Create your views here.
-def index( request ):
-  # initiate driver
-  WebDriverSingleton()
-  return render( request, "instagram/index.html" )
+from django.shortcuts import render
+
+
+def index(request):
+    user_uuid = decode_and_set_cookie(request)
+
+    if user_uuid:
+        WebDriverSingleton(str(user_uuid))
+    else:
+        pass
+
+    return render(request, "instagram/index.html")
