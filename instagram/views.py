81b541fb7096ae03f806e01fdb8d37ce7f8b9ea7
slocksert <rsdlj01@gmail.com>
2023-09-18 21:21:59 -0300

    using django request to get the token

--- instagram/views.py
+++ instagram/views.py
@@ -1,15 +1,11 @@
 from api.web_driver_singleton import WebDriverSingleton
-from instagram.utils import decode_and_set_cookie
 
 from django.shortcuts import render
 
-
 def index(request):
-    user_uuid = decode_and_set_cookie(request)
+    token = request.COOKIES.get('jwt')
 
-    if user_uuid:
-        WebDriverSingleton(str(user_uuid))
-    else:
-        pass
+    if token:
+        WebDriverSingleton(str(token))
 
     return render(request, "instagram/index.html")
