940d79d196350e62ce1c68fe8f338afc293a7f90
slocksert <rsdlj01@gmail.com>
2023-09-18 21:11:09 -0300

    changed user_session to token

--- api/web_driver_singleton.py
+++ api/web_driver_singleton.py
@@ -7,8 +7,8 @@ import platform
 class WebDriverSingleton:
   _instances = {}
 
-  def __new__( cls, user_session ):
-    if user_session not in cls._instances:
+  def __new__( cls, token ):
+    if token not in cls._instances:
       chrome_options = Options()
       chrome_options.add_argument('--headless')
       chrome_options.add_argument('--no-sandbox')
@@ -22,12 +22,12 @@ class WebDriverSingleton:
         driver = webdriver.Chrome(service=service ,options=chrome_options)
       
       driver.implicitly_wait( 15 )
-      cls._instances[user_session] = driver
+      cls._instances[token] = driver
 
-    return cls._instances[user_session]
+    return cls._instances[token]
   
   @classmethod
-  def close_driver( cls, user_session ):
-    if user_session in cls._instances:
-      cls._instances[user_session].quit()
-      del cls._instances[user_session] 
+  def close_driver( cls, token ):
+    if token in cls._instances:
+      cls._instances[token].quit()
+      del cls._instances[token]
